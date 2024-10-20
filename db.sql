5cb99824eeb877ebe3072d958cd6fdff6fa225c4
Sushil911 <Sushilbasyal30@gmail.com>
2024-10-20 13:23:43 +0000

    Add postgres database

---
+++ db.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password_hash TEXT NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE admin (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password_hash TEXT NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    image_link TEXT,
+    admin_id INT REFERENCES admin(id) ON DELETE CASCADE
+);
+
+CREATE TABLE purchases (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    course_id INT REFERENCES courses(id) ON DELETE CASCADE
+);
--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	// Loading environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading environment variables: %v", err)
+	}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error opening database: %v\n", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	fmt.Println("Successfully connected to the database")
+
 	router := mux.NewRouter()
 	userRouter := router.PathPrefix("/user").Subrouter() // user sub-router
 
@@ -28,9 +61,9 @@ func main() {
 	adminRouter.HandleFunc("/add", AddCourseContent).Methods("POST")
 
 	fmt.Println("Starting the server at port 8000")
-	err := http.ListenAndServe(":8000", router)
+	err = http.ListenAndServe(":8080", router)
 	if err != nil {
-		fmt.Println("Error while starting the server")
+		log.Fatalf("Error while starting the server")
 	}
 }
 
