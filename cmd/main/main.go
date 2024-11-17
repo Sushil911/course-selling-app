da8f6c8ee0c6d196cd2c0f57e6f6fef3cdd38a7e
Sushil911 <Sushilbasyal30@gmail.com>
2024-11-17 15:03:00 +0000

    Some minor modifications

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -12,31 +12,24 @@ import (
 )
 
 func main() {
-	// Load environment variables
 	if err := config.LoadEnv(); err != nil {
-		log.Fatalf("Error loading environment variables: %v", err)
+		log.Fatalf("Error loading environment variables %v", err)
 	}
 
-	// Initialize the database
 	db, err := db.InitDB()
 	if err != nil {
-		log.Fatalf("Error connecting to the database: %v", err)
+		log.Fatalf("Error connecting to database %v", err)
 	}
 	defer db.Close()
 
-	// Create Echo instance
 	e := echo.New()
-
-	// Default Middleware in echo for logging and recovering from any panic
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Register routes
 	routes.RegisterRoutes(e)
 
-	// Start the server
 	fmt.Println("Starting the server at port 8080")
 	if err := e.Start(":8080"); err != nil {
-		log.Fatalf("Error while starting the server: %v", err)
+		log.Fatalf("Error starting the server %v", err)
 	}
 }
